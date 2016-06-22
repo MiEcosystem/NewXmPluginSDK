7db35fdbbab801b35e8776172c18d5858bd9e329
lifeiyun <lifeiyun@xiaomi.com>
2016-06-22 18:33:37 +0800

    添加条形码扫描
    N/A
    
    Signed-off-by: lifeiyun <lifeiyun@xiaomi.com>

--- pluglib/src/main/java/com/xiaomi/smarthome/device/api/IXmPluginHostActivity.java
+++ pluglib/src/main/java/com/xiaomi/smarthome/device/api/IXmPluginHostActivity.java
@@ -627,9 +627,65 @@ public interface IXmPluginHostActivity {
      */
     public abstract void openRechargePage(int type,double latitude,double longitude);
 
+    public enum BarcodeFormat {
+
+        /** Aztec 2D barcode format. */
+        AZTEC,
+
+        /** CODABAR 1D format. */
+        CODABAR,
+
+        /** Code 39 1D format. */
+        CODE_39,
+
+        /** Code 93 1D format. */
+        CODE_93,
+
+        /** Code 128 1D format. */
+        CODE_128,
+
+        /** Data Matrix 2D barcode format. */
+        DATA_MATRIX,
+
+        /** EAN-8 1D format. */
+        EAN_8,
+
+        /** EAN-13 1D format. */
+        EAN_13,
+
+        /** ITF (Interleaved Two of Five) 1D format. */
+        ITF,
+
+        /** MaxiCode 2D barcode format. */
+        MAXICODE,
+
+        /** PDF417 format. */
+        PDF_417,
+
+        /** QR Code 2D barcode format. */
+        QR_CODE,
+
+        /** RSS 14 */
+        RSS_14,
+
+        /** RSS EXPANDED */
+        RSS_EXPANDED,
+
+        /** UPC-A 1D format. */
+        UPC_A,
+
+        /** UPC-E 1D format. */
+        UPC_E,
+
+        /** UPC/EAN extension format. Not a stand-alone format. */
+        UPC_EAN_EXTENSION
+
+    }
+
     /**
      * ApiLevel:25 跳转二维码扫描页面
-     * @param bundle 请求参数，可以穿null @see #Activity.startActivityForResult(Intent, requestCode)
+     * @param bundle 请求参数，可以传null即为扫描二维码,或者指定扫描码类型 bundle.putIntArray("barcode_format",new int[]{IXmPluginHostActivity.BarcodeFormat.QR_CODE.ordinal()});
+     * 请求逻辑@see #Activity.startActivityForResult(Intent, requestCode)
      * @param requestCode @see #Activity.startActivityForResult(Intent, requestCode)
      *
      * 结果返回到调用Activity的onActivityResult中，调用如下
@@ -638,7 +694,7 @@ public interface IXmPluginHostActivity {
      *        super.onActivityResult(requestCode, resultCode, data);
      *        if(resultCode==RESULT_OK){
      *            if(requestCode==SCAN_BARCODE){
-     *            String result = data.getStringExtra("scan_result");
+     *            String result = data.getStringExtra(ScanBarcodeActivity.SCAN_RESULT);
      *             }
      *         }
      *    }
